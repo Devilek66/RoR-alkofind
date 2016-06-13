e6e0cec283365731bf77af7e91fc013145dad5db
Devilek66 <aleksanderp1993@gmail.com>
2016-06-13 22:29:01 +0000

    add a skeleton of comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  get 'comment/index'
+
+  get 'comment/new'
+
+  get 'comment/create'
+
+  get 'comment/show'
+
+  get 'comment/destroy'
+
   devise_for :users
   root 'home#index'
   get 'kontakt' => 'home#contact', as: :contact
+  resources :comment
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
