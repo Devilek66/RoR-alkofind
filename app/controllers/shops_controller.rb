3b69a4ba7f785ff9ea594377180cbc7eb01ba355
Devilek66 <aleksanderp1993@gmail.com>
2016-06-30 10:18:57 +0000

    add database data to shops map by controller

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,17 @@
 class ShopsController < ApplicationController
   def index
+    @tmp = {
+          "url": "http://www.fancyicons.com/free-icons/232/cinema/png/32/penis_32.png",
+          "width":  32,
+          "height": 32
+        }
+    @shops = Shop.all
+    @hash = Gmaps4rails.build_markers(@shops) do |shop,marker|
+      marker.lat shop.lat
+      marker.lng shop.lng
+      marker.infowindow shop.name
+      marker.picture @tmp
+    end
   end
 
   def show
